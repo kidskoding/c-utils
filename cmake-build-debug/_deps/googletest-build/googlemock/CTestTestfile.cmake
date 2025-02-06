fa7d2077201b2dc6e68e8d23116134baf5f17ded
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-06 06:12:01 -0600

    added C project

---
+++ cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anirudh/UIUC/go-mama-27/personal-projects/c-utils/cmake-build-debug/_deps/googletest-src/googlemock
+# Build directory: /Users/anirudh/UIUC/go-mama-27/personal-projects/c-utils/cmake-build-debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
